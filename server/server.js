80ea6bae35e48b9a2fe3184d80912fd0c3342284
Raju Nayak <rajunyakpb09@gmail.com>
2026-09-23 18:26:34 +0530

    Pre-Lab - React Introduction

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "StudentMart API is running"
+    });
+});
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+
+        app.listen(process.env.PORT, () => {
+            console.log(
+                `Server running on http://localhost:${process.env.PORT}`
+            );
+        });
+    })
+    .catch((error) => {
+        console.error(
+            "MongoDB connection failed:",
+            error.message
+        );
+    });
+
+
+
