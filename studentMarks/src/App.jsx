b3226363e64ea4b5cc1bdeb141fb68a0449e32aa
Raju Nayak <rajunyakpb09@gmail.com>
2026-09-23 10:11:56 +0530

    Pre-Lab - React Introduction

--- studentMarks/src/App.jsx
+++ studentMarks/src/App.jsx
@@ -4,7 +4,8 @@ function App() {
       <h1>StudentMart</h1>
       <h2>Welcome to StudentMart</h2>
       <p>Your Online Shopping Application</p>
-      <p>Name: Raju Nayak 1AT24CS167</p>
+      <p>Name: Raju Nayak</p>
+      <p>USN:1AT24CS167</p>
     </div>
   )
 }
